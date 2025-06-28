493c4b7732393e247d22dfb3eafb00941f3cc081
Prashanthsyntax <pendemprashanth49@gmail.com>
2025-06-28 23:12:02 +0530

    added 1 more

--- hello.c
+++ hello.c
@@ -5,5 +5,6 @@ int main() {
     printf("It prints a simple message to the console.\n");
     printf("C is a powerful programming language.\n");
     printf("Prashanth Pendem\n");
+    printf("This is a simple C program.\n");
     return 0;
 }
