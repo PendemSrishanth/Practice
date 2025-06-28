7df0fea6ad8481bc516f772c51d0bce347bd31e2
Prashanthsyntax <pendemprashanth49@gmail.com>
2025-06-28 22:28:11 +0530

    Added C Features

--- hello.c
+++ hello.c
@@ -1,5 +1,8 @@
 #include<stdio.h>
 int main() {
     printf("Hello, World\n");
+    printf("This is a C program.\n");
+    printf("It prints a simple message to the console.\n");
+    printf("C is a powerful programming language.\n");
     return 0;
 }
